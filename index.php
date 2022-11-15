627cb88c75398de2894e0f133e5d03f547a90639
shallidesu <shallitriana@gmail.com>
2022-11-15 09:31:24 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -7,5 +7,8 @@
         <h1>Hello World!</h1>
 
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, deleniti non est quam excepturi asperiores ea quis? Quaerat, enim! Itaque sequi necessitatibus, commodi nam a facere nostrum! Doloremque, porro cum.</p>
+        <ul>
+            <li>satu</li>
+</ul>
     </body>
 </html>
